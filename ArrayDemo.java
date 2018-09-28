8f23c5c73a8a2b1fd248b01406ba279f8a47a3ea
DarthChurros <tnarayan00@stuy.edu>
2018-09-27 20:25:32 -0400

    added call between print functions

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -1,22 +1,18 @@
 public class ArrayDemo {
   public static void printArray (int[] ary) {
-    System.out.print("[");
+    System.out.print("{");
     for (int i = 0; i < ary.length; i++) {
       System.out.print(ary[i] + ", ");
     }
-    System.out.print("\b\b]\n");
+    System.out.print("\b\b}\n");
   }
   public static void printArray(int[][] ary) {
-    System.out.println("[");
-    for (int i = 0; i < ary.length; i++) {
-      System.out.print("[");
-      for (int j = 0; j < ary[0].length; j++) {
-      System.out.print(ary[i][j] + ", ");
+    System.out.print("{");
+    for (int j = 0; j < ary.length; j++) {
+      printArray(ary[j]);
     }
-    System.out.print("\b\b]\n");
+  System.out.print("\b\b}\n");
   }
-  System.out.print("\b\b]\n");
-}
   public static void main (String[] args) {
     int[] x = new int[7];
     int[][] y = new int[3][10];
